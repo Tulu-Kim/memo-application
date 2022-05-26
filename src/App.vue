eebf114106dc6d267743d8d608447e822e1f7f77
gkeat77 <gkeat777@naver.com>
2022-05-26 17:31:15 +0900

    test

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <app-header/>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components:{
+    AppHeader
+  }
+}
+</script>
+
+<style>
+/* reset.css 파일을 가져온다.*/
+@import "./styles/reset.css";
+#app{
+  width: 560px;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/MemoApp.vue
@@ -0,0 +1,31 @@
+<!-- src/components/MemoApp.vue-->
+<template>
+  <div class="memo-app">
+      <memo-form />
+      <memo />
+  </div>
+</template>
+
+<script>
+import MemoForm from './MemoForm.vue';
+import Memo from './Memo.vue';
+
+export default {
+    name: 'MemoApp',
+    data () {
+        return{
+            memos: [],
+        };
+    },
+    created (){
+        //1.만약 기존에 추가된 localStorage에 데이터가 있다면 created훅에서 localStorage의 데이터를 컴포넌트 내의 memos 데이터에 넣어주고, 그렇지 않다면 그대로 빈 배열로 초기화한다.
+        this.memos = localStorage.memos ? JSON.parse(localStorage.memos) : [];
+
+    }
+
+}
+</script>
+
+<style>
+
+</style>
